aa0cd75a99133f2ed9b2cf9c9a0f66aa56dad274
unknown <900150@proway.treina>
2019-11-13 09:05:45 -0300

    aula7

---
+++ aula7.py
@@ -0,0 +1,23 @@
+# Aula7 - 13/11/2019
+# Lista
+# Indicalização de uma variavel do tipo vazia
+lista = []
+# indicação de uma variavel lista, com elementos
+lista = ["Marcela," 'Nicole','Matheus']
+# lista de inteiros 
+lista3 = [1,2,3,5]
+
+
+lista = []
+lista2 = ["Marcela," 'Nicole','Matheus']
+lista3 = [1,2,3,5]
+
+print(lista)
+print(lista2)
+print(lista3)
+
+lista.append(lista2)
+lista.append(lista3)
+
+print(lista)
+
